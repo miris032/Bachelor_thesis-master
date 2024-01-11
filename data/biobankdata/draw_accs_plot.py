4200fccfb2abc41c9fac62a3ded4f4f48ca293ea
miris <pastellest@gmail.com>
2024-01-11 16:35:24 +0100

    some changes

---
+++ data/biobankdata/draw_accs_plot.py
@@ -0,0 +1,92 @@
+import csv
+import os
+from pathlib import Path
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib.dates as mdates
+
+root = Path(__file__).resolve().parent.parent
+
+
+def only_retain_last_1(input_list):
+    result_list = []
+    current_sequence = 0
+
+    for element in input_list:
+        if element == 1:
+            current_sequence += 1
+        else:
+            current_sequence = 0
+
+        result_list.append(1 if current_sequence == 1 else 0)
+
+    return result_list
+
+
+# 输入由0和1组成的list，返回所有1的位置
+def getPosition(input_list):
+    drift_pos = []
+    for i, values in enumerate(input_list):
+        if values == 1:
+            drift_pos.append(i)
+    return drift_pos
+
+
+def drawPlot(file, d, a):
+    filename = os.path.basename(file)
+    data = np.asarray(pd.read_csv(f'{file}' + '.csv', delimiter=",", header=None))
+    data = data[1:]
+    data = np.array(data)
+    data = np.transpose(data)
+
+    data1 = data[[1, 2, 3, 4, 5, 6, 7, 8, 9], :]
+    data1 = data1.astype(float)
+
+    time = data[0, :]
+    time = [timestamp.split()[0] + ' ' + timestamp.split()[1] for timestamp in time]
+
+    # The first colomn of time in accs.csv, accs_after_filling.csv and accs_after_filling_and_binning.csv
+    # should be "%Y-%m-%d %H:%M:%S.%f%z", although the %Y-%m-%ds are not true, but we need this format.
+
+    input_format = "%Y-%m-%d %H:%M:%S.%f%z"
+    output_format = "%Y-%m-%d %H:%M:%S"
+    time = [datetime.strptime(date, input_format).strftime(output_format) for date in time]
+    x_time = [datetime.strptime(d, '%Y-%m-%d %H:%M:%S') for d in time]
+
+    pkl1 = pd.read_pickle(
+        f'{root}/../results/exp_2023_ijcai/' + f'{file}' + '_and_binning' + '/full-1/slidshap_drifts_bi_predictions_Realworld_' + f'{file}' + '_and_binning' + '_windowlength' + f'{d}' + '_overlap' + f'{a}' + '.pkl')
+    content1 = list(pkl1.values())[0]
+    print(f'slidSHAPs predications: {content1}')
+    print(f'length of slidSHAPs predications: {len(content1)}')
+    print(f'length of original time series: {len(data[0])}')
+
+    # 通过插值将长度扩展为目标长度
+    content1 = np.array(content1)
+    new_indices = np.linspace(0, len(content1) - 1, len(data[0]), endpoint=True).astype(int)
+    timeseries_predictions = content1[new_indices]
+    print(f'drift position on the time series axis: {getPosition(timeseries_predictions)}')
+    # timeseries_predictions = only_retain_last_1(timeseries_predictions)
+    # print(f'drift position on the time series axis: {getPosition(timeseries_predictions)}')
+
+    plt.figure(figsize=(15, 3))
+    plt.suptitle(f'{filename}', fontsize=16, fontweight='bold')
+    header = ('acc1', 'acc2', 'acc3', 'acc4', 'acc5', 'acc6', 'acc7', 'acc8', 'acc9')
+    for i, row in enumerate(data1):
+        plt.plot(x_time, row, label=header[i], linewidth=0.2, alpha=0.5)
+    for i, value in enumerate(timeseries_predictions):
+        if value == 1:
+            plt.axvline(x=x_time[i], color='red', linestyle='-', linewidth=0.8, alpha=0.01)
+    plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper left')
+
+    plt.show()
+    # plt.savefig('../biobankdata_result/accs_after_filling/' + f'{filename}.png')
+    # plt.close()
+
+
+if __name__ == '__main__':
+    # draw accs_after_filling, not accs_after_filling_and_binning as the original ts
+    drawPlot('accs_after_filling', 200, 140)
--- data/ticker_data/draw_mix_ticker.py
+++ data/ticker_data/draw_mix_ticker.py
@@ -16,10 +16,11 @@ root = Path(__file__).resolve().parent.parent
 def draw_plots(file):
     filename = os.path.basename(file)
 
-    data = np.asarray(pd.read_csv(f'{root}/ticker_data/columns_hourly/' + f'{file}' + '.csv', delimiter=",", header=None))
+    data = np.asarray(pd.read_csv(f'{root}/ticker_data/' + f'{file}' + '.csv', delimiter=",", header=None))
     data = np.transpose(data)
     time = data[0, 1:]
-    data = data[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 1:]
+    # data = data[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 1:]
+    data = data[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1:]
     data = data.astype(float)
     input_format = "%Y-%m-%d %H:%M:%S%z"
     output_format = "%Y-%m-%d %H:%M:%S%z"
@@ -32,11 +33,12 @@ def draw_plots(file):
     colors = ['#FF9933', '#FF6666', '#66FF66', '#6666FF', '#996699', '#66CCCC']
 
     for i, row in enumerate(data):
-        plt.plot(x_time2, row, label=header[i], linewidth=0.3)
+        # plt.plot(x_time2, row, label=header[i], linewidth=0.3)
+        plt.plot(row, label=header[i], linewidth=0.3)
 
-    plt.suptitle(f'{filename}' + '_hourly', fontsize=16, fontweight='bold')
+    plt.suptitle(f'{filename}' + '', fontsize=16, fontweight='bold')
     plt.subplots_adjust(hspace=0.8)
-    plt.legend(loc=2, bbox_to_anchor=(1, 1))
+    plt.legend(loc=2, bbox_to_anchor=(1, 1), prop={'size': 6})
     # plt.legend(loc=4)
 
     plt.show()
@@ -45,4 +47,4 @@ def draw_plots(file):
 
 
 if __name__ == '__main__':
-    draw_plots('High')
+    draw_plots('High_hourly_events')
