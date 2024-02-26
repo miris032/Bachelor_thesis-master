8e39098ccbd7015b6e1b0bf52adba37cf3a79ebe
miris <pastellest@gmail.com>
2024-02-26 20:50:42 +0100

    sudden drifts

--- src/events_insertion.py
+++ src/events_insertion.py
@@ -1,6 +1,7 @@
 import random
 import sys
-
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -8,14 +9,16 @@ import matplotlib.pyplot as plt
 root = Path(__file__).resolve().parent.parent
 
 
-def random_walk(initial_price, num_steps, volatilities):
+def random_walk(initial_price, end_price, num_steps, volatilises):
+    # np.random.seed(42)  # 设置随机数种子
     prices = [initial_price]
     for i in range(num_steps):
-        drift = 0  # 漂移项，可以根据需要添加
-        shock = np.random.normal(loc=0, scale=volatilities[i % len(volatilities)])  # 随机波动项，代表价格的波动
+        drift = (end_price - initial_price) / 210  # 漂移项，可以根据需要添加
+        # shock = np.random.normal(loc=0, scale=volatilises[i % len(volatilises)])
+        shock = np.random.normal(loc=0, scale=volatilises[i % len(volatilises)])  # 随机波动项，代表价格的波动
         price = prices[-1] + drift + shock
         prices.append(price)
-    return prices
+    return prices[1:]
 
 
 def random_triangular(initial_price, num_steps, volatilities):
@@ -36,7 +39,24 @@ def random_triangular(initial_price, num_steps, volatilities):
     return prices
 
 
-def biobankdata_insertion(file, insert_pos, row_num):
+def random_sin(initial_point, num_cycles, num_points_per_cycle):
+    total_points = num_cycles * num_points_per_cycle
+    x = np.linspace(100, 2 * np.pi * num_cycles, total_points)
+    y = (np.sin(x)).astype(int) + np.random.uniform(1.5, 3.5, total_points).round(3)
+    y = [float(point + initial_point) for point in y]
+    return y
+
+
+def gaussian_drift_generation(initial_point, number, l, nu):
+    kernel = Matern(length_scale=l, nu=nu)
+    gp = GaussianProcessRegressor(kernel=kernel)
+    y = gp.sample_y(np.linspace(0, 10, number)[:, np.newaxis])
+    y = [float(point + initial_point) for point in y]
+    y = [point * 1.5 for point in y]
+    return y
+
+
+def biobankdata_insertion(file, insert_pos, row_num, event_type):
 
     if file.startswith('dataset'):
         dataset_1_to_9 = True
@@ -55,7 +75,8 @@ def biobankdata_insertion(file, insert_pos, row_num):
     insert_pos_after = []
     seed = 42
 
-    for pos in insert_pos:
+    for pos_index, pos in enumerate(insert_pos):
+        print(pos_index)
 
         np.random.seed(seed)
         pos += insert_pos.index(pos) * row_num
@@ -63,55 +84,94 @@ def biobankdata_insertion(file, insert_pos, row_num):
         tmp_arr = np.empty((column_num, row_num))
 
         for i in range(column_num):
-            if not dataset_1_to_9 or (dataset_1_to_9 and i == 0):
-                np.random.seed(seed)
-                min_value_30th = np.sort(data[i][pos-1000: pos])[10]
-                max_value_30th = np.sort(data[i][pos-1000: pos])[-10]
+            np.random.seed(seed)
 
-                '''mean = (min_value_30th + max_value_30th) / 2 - 30
-                std_dev = (max_value_30th - min_value_30th) / 2
-                std_dev *= 2'''
+            # single
+            if dataset_1_to_9:
+                # acc column
+                if i == 0:
+                    # sudden
+                    if event_type == 'sudden':
+                        y = random_sin(250, 25, 30)
+                    # incremental
+                    else:
+                        y = np.empty((0,))
+                        for _ in range(int(row_num / 10)):
+                            min_value_30th = np.sort(data[i][pos - 1000: pos])[10]
+                            max_value_30th = np.sort(data[i][pos - 1000: pos])[-10]
+                            mean = max_value_30th / random.uniform(2, 10)
 
-                y = np.empty((0,))
-                for _ in range(int(row_num/10)):
-                    mean = max_value_30th / random.uniform(2, 10)
+                            # 生成0到1之间的随机数
+                            random_prob = random.random()
 
-                    # 生成0到1之间的随机数
-                    random_prob = random.random()
+                            # 根据随机数的值来决定增大或减小std_dev
+                            if random_prob < 0.8:  # 80%的概率
+                                std_dev = max_value_30th / random.uniform(15, 20)
+                            else:  # 20%的概率
+                                std_dev = max_value_30th / random.uniform(1, 3)
 
-                    # 根据随机数的值来决定增大或减小std_dev
-                    if random_prob < 0.8:  # 80%的概率
-                        std_dev = max_value_30th / random.uniform(15, 20)
-                    else:  # 20%的概率
-                        std_dev = max_value_30th / random.uniform(1, 3)
+                            y1 = np.abs(np.random.normal(mean, std_dev, 10))
+                            y = np.hstack((y, y1))
 
-                    y1 = np.abs(np.random.normal(mean, std_dev, 10))
-                    y = np.hstack((y, y1))
+                # MET column
+                elif i == 5:
+                    # sudden
+                    if event_type == 'sudden':
+                        y = np.mean(data[5] + random.uniform(5, 10))
+                    # incremental
+                    else:
+                        y = np.mean(data[5])
 
-                '''num_large_values = int(row_num * 0.01)  # 偶尔出现较大值的数量
-                large_values_indices = np.random.choice(np.arange(row_num), size=num_large_values, replace=False)  # 随机选择插入位置
-                large_values = np.random.uniform(0, max_value_30th*4, size=num_large_values)  # 在指定范围内生成均匀分布的大值
-                y[large_values_indices] = large_values  # 将大值替换掉原始数据的一部分'''
+                # classification columns
+                elif i == 1:
+                    y = [1 for i in range(row_num)]
+                elif i == 2:
+                    y = [0 for i in range(row_num)]
+                elif i == 3:
+                    y = [0 for i in range(row_num)]
+                elif i == 4:
+                    y = [0 for i in range(row_num)]
 
-            elif dataset_1_to_9 and i == 5:
-                y = np.mean(data[5])
-            elif dataset_1_to_9 and i == 1:
-                y = [1 if i < row_num/5 else 0 for i in range(row_num)]
-            elif dataset_1_to_9 and i == 2:
-                y = [0 if i < row_num*2/5 else 1 if i < row_num*3/5 else 0 for i in range(row_num)]
-            elif dataset_1_to_9 and i == 3:
-                y = [0 if i < row_num*3/5 else 1 if i < row_num*4/5 else 0 for i in range(row_num)]
-            elif dataset_1_to_9 and i == 4:
-                y = [1 if i > row_num*4/5 else 0 for i in range(row_num)]
+            # accs
+            else:
+                # sudden
+                if event_type == 'sudden':
+                    y = random_sin(1000, 25, 30)
+
+                # incremental
+                else:
+                    y = np.empty((0,))
+                    for _ in range(int(row_num / 10)):
+                        min_value_30th = np.sort(data[i][pos - 1000: pos])[10]
+                        max_value_30th = np.sort(data[i][pos - 1000: pos])[-10]
+                        mean = max_value_30th / random.uniform(2, 10)
+
+                        # 生成0到1之间的随机数
+                        random_prob = random.random()
+
+                        # 根据随机数的值来决定增大或减小std_dev
+                        if random_prob < 0.8:  # 80%的概率
+                            std_dev = max_value_30th / random.uniform(15, 20)
+                        else:  # 20%的概率
+                            std_dev = max_value_30th / random.uniform(1, 3)
+
+                        y1 = np.abs(np.random.normal(mean, std_dev, 10))
+                        y = np.hstack((y, y1))
+
             tmp_arr[i, :] = y
-
         data = np.insert(data, pos, tmp_arr.transpose(), axis=1)
-
     data = data.transpose()
+
     if not dataset_1_to_9:
-        np.savetxt('accs(events).csv', data, delimiter=",", fmt='%s')
+        if event_type == 'sudden':
+            np.savetxt('../data/biobankdata/accs(events2).csv', data, delimiter=",", fmt='%s')
+        else:
+            np.savetxt('../data/biobankdata/accs(events).csv', data, delimiter=",", fmt='%s')
     else:
-        np.savetxt('dataset1-timeSeries(events).csv', data, delimiter=",", fmt='%s')
+        if event_type == 'sudden':
+            np.savetxt('../data/biobankdata/dataset1-timeSeries(events2).csv', data, delimiter=",", fmt='%s')
+        else:
+            np.savetxt('../data/biobankdata/dataset1-timeSeries(events).csv', data, delimiter=",", fmt='%s')
 
     # 绘制折线图
     plt.figure(figsize=(15, 3))
@@ -131,18 +191,13 @@ def biobankdata_insertion(file, insert_pos, row_num):
     plt.show()
 
 
-def stockdata_insertion(file, insert_pos, row_num):
+def stockdata_insertion(file, insert_pos, row_num, event_type):
 
     if any(file.startswith(prefix) for prefix in ('Open', 'Close', 'High', 'Low', 'Volume', 'Adj Close')):
         mix = True
+        df = pd.read_csv(f'{root}/data/ticker_data/{file}.csv', delimiter=",", header=None)
     else:
         mix = False
-
-    # mix
-    if mix:
-        df = pd.read_csv(f'{root}/data/ticker_data/{file}.csv', delimiter=",", header=None)
-    # single stock
-    else:
         time_category = file.split('_')[1]
         file_name = file.split('_')[0]
         df = pd.read_csv(f'{root}/data/ticker_data/{time_category}/{file_name}.csv', delimiter=",", header=None)
@@ -152,66 +207,80 @@ def stockdata_insertion(file, insert_pos, row_num):
     data = np.asarray(df.iloc[1:, 1:].T.astype(float))
 
     column_num = data.shape[0]
-    insert_pos_after = []
     seed = 42
 
-    for pos in insert_pos:
+    for pos_index, pos in enumerate(insert_pos):
         #np.random.seed(pos)
-        pos += insert_pos.index(pos) * row_num
+        '''pos += insert_pos.index(pos) * row_num
         insert_pos_after.append(pos)
-        tmp_arr = np.empty((column_num, row_num))
-        for i in range(column_num):
-            np.random.seed(seed+i)
+        tmp_arr = np.empty((column_num, row_num))'''
+        for column in range(column_num):
+            #np.random.seed(seed+column)
+            np.random.seed(42 + pos_index)  # 设置随机数种子
 
             # mix
             if mix:
-                if i == 6 or i == 7 or i == 8:
-                    y = random_walk(data[i][pos], row_num - 1, (0.05, 0.075))
-                elif i == 0 or i == 4 or i == 9:
-                    y = random_walk(data[i][pos], row_num - 1, (0.2, 0.4, 0.6))
+                if column == 6 or column == 7 or column == 8:
+                    v = (0.05, 0.075)
+                elif column == 0 or column == 4 or column == 9:
+                    v = (0.2, 0.4, 0.6)
                 else:
-                    y = random_walk(data[i][pos], row_num - 1, (0.9, 1.2, 1.5))
+                    v = (0.9, 1.2, 1.5)
+
+                # sudden
+                if event_type == 'sudden':
+                    data[column][pos: pos + row_num] = random_walk(data[column][pos], data[column][pos + row_num], row_num, v)
+                # incremental
+                else: # TODO
+                    '''y = random_walk(data[1][pos], row_num - 1, (0.9, 1.2, 1.5))
+                    y = [point + 250 for point in y]'''
+                    # y = random_sin(400, 4, 50)
+                    change_pos = 20 * column
+                    y1 = data[column][pos: pos + change_pos].tolist()
+                    y2 = random_walk(data[column][pos + change_pos], data[column][pos + row_num], (row_num - change_pos), v)
+                    data[column][pos: pos + row_num] = y1 + y2
+
+
             # single
             else:
-                if time_category == 'daily':
-                    if i < 5:
-                        y = random_walk(data[i][pos], row_num-1, 0.5)
-                    else:
-                        y = np.random.uniform(low=np.sort(data[i][pos-400: pos])[30], high=np.sort(data[i][pos-400: pos])[-30],
-                                              size=row_num)
-                elif time_category == 'hourly':
-                    if i < 4:
-                        y = random_walk(data[i][pos], row_num-1, (0.2, 0.3, 0.4))
-                    else:
-                        y = np.random.uniform(low=np.sort(data[i][pos-400: pos])[30], high=np.sort(data[i][pos-400: pos])[-30],
-                                              size=row_num)
-                    y = [element + 5 for element in y]
-                    y = np.round(y, 5)
+                if time_category == 'daily' or time_category == 'hourly':
+                    v = (0.75, 1, 1.25)
                 # minutely
                 else:
-                    if i < 4:
-                        y = random_triangular(data[i][pos], row_num-1, (0.2, 0.3, 0.4))
-                    else:
-                        y = np.random.uniform(low=np.sort(data[i][pos-100: pos])[5], high=np.sort(data[i][pos-100: pos])[-5],
-                                              size=row_num)
-            tmp_arr[i, :] = y
-        data = np.insert(data, pos, tmp_arr.transpose(), axis=1)
-        seed += 1
+                    v = (0.02, 0.03, 0.04)
+
+                # sudden
+                if event_type == 'sudden':
+                    data[column][pos: pos+row_num] = random_walk(data[column][pos], data[column][pos + row_num], row_num, v)
+                # incremental
+                else:
+                    change_pos = int(column * row_num / (len(insert_pos) - 1))
+                    y1 = data[column][pos: pos + change_pos].tolist()
+                    y2 = random_walk(data[column][pos + change_pos], data[column][pos + row_num], row_num - change_pos, v)
+                    data[column][pos: pos+row_num] = y1 + y2
 
     data = data.transpose()
     if mix:
-        np.savetxt(f'Open_hourly(events).csv', data, delimiter=",", fmt='%s')
+        if event_type == 'sudden':
+            np.savetxt('../data/ticker_data/Open_hourly(events2).csv', data, delimiter=",", fmt='%s')
+        else:
+            np.savetxt('../data/ticker_data/Open_hourly(events).csv', data, delimiter=",", fmt='%s')
+
     else:
-        np.savetxt(f'BMW.DE(events)_{time_category}.csv', data, delimiter=",", fmt='%s')
+        if event_type == 'sudden':
+            np.savetxt(f'../data/ticker_data/{time_category}/BMW.DE(events2).csv', data, delimiter=",", fmt='%s')
+        else:
+            np.savetxt(f'../data/ticker_data/{time_category}/BMW.DE(events).csv', data, delimiter=",", fmt='%s')
+
 
     # 绘制折线图
     plt.figure(figsize=(15, 3))
-    for i in range(column_num):
-        plt.plot(data[:, i], label=f'Column {i + 1}', linewidth=0.2, alpha=0.5)
+    for column in range(column_num):
+        plt.plot(data[:, column], label=f'Column {column + 1}', linewidth=0.2, alpha=0.5)
 
     # 使用 fill_between 函数填充插入数据的区域
     ylim = plt.gca().get_ylim()
-    for pos in insert_pos_after:
+    for pos in insert_pos:
         print(f'{pos + 1} - {pos + row_num}')
         plt.fill_between(range(pos + 1, pos + row_num + 1), ylim[0], ylim[1], color='gray', hatch='///', alpha=0.1)
 
@@ -224,11 +293,11 @@ def stockdata_insertion(file, insert_pos, row_num):
 
 if __name__ == '__main__':
 
-    biobankdata_insertion('dataset1-timeSeries', (2500, 6000, 9500, 13000, 16500), 1500)
-    # biobankdata_insertion('accs_filled', (1500, 5000, 8500, 12000, 15500), 1000)
+    # biobankdata_insertion('dataset1-timeSeries_filled', (2500, 6000, 9500, 13000, 16500), 750, 'incremental') # incremental
+    # biobankdata_insertion('accs_filled', (2500, 6000, 9500, 13000, 16500), 750, 'sudden') # incremental
 
 
-    # stockdata_insertion('BMW.DE_hourly', (1000, 2000, 3000, 4000, 5000), 500)
-    # stockdata_insertion('BMW.DE_minutely', (300, 600, 900, 1200, 1500), 200)
-
-    # stockdata_insertion('Open_hourly', (1000, 2000, 3000, 4000, 5000), 500)
+    # stockdata_insertion('BMW.DE_daily', (1200, 2400, 3600, 4800, 6000), 250, 'sudden')
+    # stockdata_insertion('BMW.DE_hourly', (1000, 2000, 3000, 4000, 5000), 200, 'sudden')
+    # stockdata_insertion('BMW.DE_minutely', (300, 600, 900, 1200, 1500), 70, 'sudden')
+    stockdata_insertion('Open_hourly', (1000, 2000, 3000, 4000, 5000), 200, 'sudden') # incremental
