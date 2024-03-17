f00e0b9c0369d970b608c524fc9ad206b9c79d02
miris <pastellest@gmail.com>
2024-03-17 14:37:44 +0100

    back up

--- src/events_insertion.py
+++ src/events_insertion.py
@@ -56,6 +56,22 @@ def gaussian_drift_generation(initial_point, number, l, nu):
     return y
 
 
+def random_biobank(row_num, max_value_30th):
+    y = np.empty((0,))
+    for _ in range(int(row_num / 10)):
+        mean = max_value_30th / random.uniform(2, 10)
+        # 生成0到1之间的随机数
+        random_prob = random.random()
+        # 根据随机数的值来决定增大或减小std_dev
+        if random_prob < 0.8:  # 80%的概率
+            std_dev = max_value_30th / random.uniform(15, 20)
+        else:  # 20%的概率
+            std_dev = max_value_30th / random.uniform(1, 3)
+        y1 = np.abs(np.random.normal(mean, std_dev, 10))
+        y = np.hstack((y, y1))
+    return y
+
+
 def biobankdata_insertion(file, insert_pos, row_num, event_type):
 
     if file.startswith('dataset'):
@@ -72,24 +88,17 @@ def biobankdata_insertion(file, insert_pos, row_num, event_type):
     print(data.shape)
 
     column_num = data.shape[0]
-    insert_pos_after = []
     seed = 42
 
     for pos_index, pos in enumerate(insert_pos):
-        print(pos_index)
-
-        np.random.seed(seed)
-        pos += insert_pos.index(pos) * row_num
-        insert_pos_after.append(pos)
-        tmp_arr = np.empty((column_num, row_num))
-
-        for i in range(column_num):
+        for column in range(column_num):
             np.random.seed(seed)
+            max_value_30th = np.sort(data[column][pos - 1000: pos])[-10]
 
             # single
             if dataset_1_to_9:
                 # acc column
-                if i == 0:
+                if column == 0:
                     # sudden
                     if event_type == 'sudden':
                         y = random_sin(250, 25, 30)
@@ -97,8 +106,8 @@ def biobankdata_insertion(file, insert_pos, row_num, event_type):
                     else:
                         y = np.empty((0,))
                         for _ in range(int(row_num / 10)):
-                            min_value_30th = np.sort(data[i][pos - 1000: pos])[10]
-                            max_value_30th = np.sort(data[i][pos - 1000: pos])[-10]
+                            min_value_30th = np.sort(data[column][pos - 1000: pos])[10]
+                            max_value_30th = np.sort(data[column][pos - 1000: pos])[-10]
                             mean = max_value_30th / random.uniform(2, 10)
 
                             # 生成0到1之间的随机数
@@ -114,7 +123,7 @@ def biobankdata_insertion(file, insert_pos, row_num, event_type):
                             y = np.hstack((y, y1))
 
                 # MET column
-                elif i == 5:
+                elif column == 5:
                     # sudden
                     if event_type == 'sudden':
                         y = np.mean(data[5] + random.uniform(5, 10))
@@ -123,45 +132,32 @@ def biobankdata_insertion(file, insert_pos, row_num, event_type):
                         y = np.mean(data[5])
 
                 # classification columns
-                elif i == 1:
+                elif column == 1:
                     y = [1 for i in range(row_num)]
-                elif i == 2:
+                elif column == 2:
                     y = [0 for i in range(row_num)]
-                elif i == 3:
+                elif column == 3:
                     y = [0 for i in range(row_num)]
-                elif i == 4:
+                elif column == 4:
                     y = [0 for i in range(row_num)]
 
             # accs
             else:
                 # sudden
                 if event_type == 'sudden':
-                    y = random_sin(1000, 25, 30)
+                    data[column][pos: pos + row_num] = random_biobank(row_num, max_value_30th)
 
                 # incremental
                 else:
-                    y = np.empty((0,))
-                    for _ in range(int(row_num / 10)):
-                        min_value_30th = np.sort(data[i][pos - 1000: pos])[10]
-                        max_value_30th = np.sort(data[i][pos - 1000: pos])[-10]
-                        mean = max_value_30th / random.uniform(2, 10)
+                    change_pos = 80 * column
+                    y1 = data[column][pos: pos + change_pos].tolist()
+                    y2 = random_biobank((row_num-change_pos), max_value_30th).tolist()
+                    print(f'y1 = data[{column}][{pos}: {pos} + {change_pos}].tolist()')
+                    print(f'y2 = random_biobank(({row_num}-{change_pos}), {max_value_30th}).tolist()')
+                    print()
+                    data[column][pos: pos + row_num] = y1 + y2
 
-                        # 生成0到1之间的随机数
-                        random_prob = random.random()
-
-                        # 根据随机数的值来决定增大或减小std_dev
-                        if random_prob < 0.8:  # 80%的概率
-                            std_dev = max_value_30th / random.uniform(15, 20)
-                        else:  # 20%的概率
-                            std_dev = max_value_30th / random.uniform(1, 3)
-
-                        y1 = np.abs(np.random.normal(mean, std_dev, 10))
-                        y = np.hstack((y, y1))
-
-            tmp_arr[i, :] = y
-        data = np.insert(data, pos, tmp_arr.transpose(), axis=1)
     data = data.transpose()
-
     if not dataset_1_to_9:
         if event_type == 'sudden':
             np.savetxt('../data/biobankdata/accs(events2).csv', data, delimiter=",", fmt='%s')
@@ -175,12 +171,12 @@ def biobankdata_insertion(file, insert_pos, row_num, event_type):
 
     # 绘制折线图
     plt.figure(figsize=(15, 3))
-    for i in range(column_num):
-        plt.plot(data[:, i], label=f'Column {i + 1}', linewidth=0.2, alpha=0.5)
+    for column in range(column_num):
+        plt.plot(data[:, column], label=f'Column {column + 1}', linewidth=0.2, alpha=0.5)
 
     # 使用 fill_between 函数填充插入数据的区域
     ylim = plt.gca().get_ylim()
-    for pos in insert_pos_after:
+    for pos in insert_pos:
         print(f'{pos + 1} - {pos + row_num}')
         plt.fill_between(range(pos + 1, pos + row_num + 1), ylim[0], ylim[1], color='gray', hatch='///', alpha=0.1)
 
@@ -231,7 +227,7 @@ def stockdata_insertion(file, insert_pos, row_num, event_type):
                 if event_type == 'sudden':
                     data[column][pos: pos + row_num] = random_walk(data[column][pos], data[column][pos + row_num], row_num, v)
                 # incremental
-                else: # TODO
+                else:
                     '''y = random_walk(data[1][pos], row_num - 1, (0.9, 1.2, 1.5))
                     y = [point + 250 for point in y]'''
                     # y = random_sin(400, 4, 50)
@@ -294,10 +290,10 @@ def stockdata_insertion(file, insert_pos, row_num, event_type):
 if __name__ == '__main__':
 
     # biobankdata_insertion('dataset1-timeSeries_filled', (2500, 6000, 9500, 13000, 16500), 750, 'incremental') # incremental
-    # biobankdata_insertion('accs_filled', (2500, 6000, 9500, 13000, 16500), 750, 'sudden') # incremental
+    # biobankdata_insertion('accs_filled', (3000, 6000, 9000, 12000, 15000), 750, 'sudden') # incremental
 
 
-    # stockdata_insertion('BMW.DE_daily', (1200, 2400, 3600, 4800, 6000), 250, 'sudden')
-    # stockdata_insertion('BMW.DE_hourly', (1000, 2000, 3000, 4000, 5000), 200, 'sudden')
-    # stockdata_insertion('BMW.DE_minutely', (300, 600, 900, 1200, 1500), 70, 'sudden')
-    stockdata_insertion('Open_hourly', (1000, 2000, 3000, 4000, 5000), 200, 'sudden') # incremental
+    # stockdata_insertion('BMW.DE_daily', (1200, 2400, 3600, 4800, 6000), 250, 'incremental')
+    # stockdata_insertion('BMW.DE_hourly', (1000, 2000, 3000, 4000, 5000), 200, 'incremental')
+    stockdata_insertion('BMW.DE_minutely', (300, 600, 900, 1200, 1500), 70, 'incremental')
+    # stockdata_insertion('Open_hourly', (1000, 2000, 3000, 4000, 5000), 200, 'sudden') # incremental
